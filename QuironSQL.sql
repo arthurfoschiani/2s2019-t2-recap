6b6f1872329636337da286f15e49356bb644f2b5
Arthur Foschiani De Souza <43127523890@info.local>
2019-12-18 08:45:40 -0300

    up

--- QuironSQL.sql
+++ QuironSQL.sql
@@ -28,3 +28,6 @@ values ('Luan Kauê Antonio da Rocha', '1984-12-10', '07259809825', 2),
 ('Isabel Malu Aline Dias', '1984-08-11', '75259870840', null),
 ('Luan Otávio Henrique da Rosa', '1984-09-19', '30241818818', 1),
 ('Marcos Mateus Rocha', '1984-01-26' , '49117819806', 1);
+
+ALTER TABLE Doutores
+ADD CONSTRAINT UC_Crm UNIQUE (Crm,CrmUf);
